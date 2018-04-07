4fd09c3471f49f702ea8b0ca31add9ea598a610c
Jaime Rivera <jriverarojas@gmail.com>
2018-04-07 10:15:10 -0400

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -46,7 +46,7 @@ def instalar_pxp():
 		
 # instalacion del php  #
 	sudo("yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm")
-	sudo("yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm")
+	sudo("yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm")
 	sudo("yum -y install yum-utils")
 	sudo("yum-config-manager --enable remi-php72")
 	
